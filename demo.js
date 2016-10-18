96455f9ce8d164646dc6d152a2949a4941bbd593
wuhezhi <1297817243@qq.com>
2016-10-18 17:27:16 +0800

    v1.0.8

--- demo.js
+++ demo.js
@@ -10,10 +10,12 @@ var assert = require("assert");
 
 var port = 5500;
 var socket = new net.Socket();
+var result = socket.connect(port,"127.0.0.1");
 var barcode = "";
 var scan_start = 1;
 var scan_stop = 2;
 var status = false;
+var BUTTONNUM = 1;
 
 /* pagestack */
 var pagestack = new Array();
@@ -31,8 +33,7 @@ var UP = 103;
 var DOWM = 108;
 var LEFT = 105;
 var RIGHT = 106;
-
-var result = socket.connect(port,"127.0.0.1");
+var ENTER = 28;
 
 var date = new Date();
 var today = date.toLocaleDateString();
@@ -72,7 +73,6 @@ socket.on("end",function(){
 })
 
 gui.on('onPaint',function(hdc){
-
     pageindex = pagestack[pagestack.length-1];
     console.log(pageindex);
     switch(pageindex){
@@ -123,29 +123,70 @@ gui.on('onKeydown',function(key){
 	        }
 		    break;
 
-		    case ALPHA:
+		    /*case ALPHA:
 		    if(pageindex != 2){
 		    	page2();
 		        pagestack.push(2);
 		        pageindex = 2;
 		    }
 		   // pageindex = 2;
-		    break;
+		    break; */
 
 	        case BACKSPACE:
-	        if(pagestack.length > 1){
-	        	pagestack.pop();
+	        {
+	        	if(pagestack.length > 1){
+	        		pagestack.pop();
+	        	}
+	        	gui.emit('onPaint');
+            	break;
 	        }
-	        gui.emit('onPaint');
-            break;
+	        
 
-            case UP:
+            //case UP:
             case LEFT:
+            {
+            	if(buttonnum > 0)
+            		buttonnum--;
+            	var hdc = gui.getclientdc();
+            	if(buttonnum > 0){
+            		gui.selectbutton(hdc);
+            	}else{
+            		console.log("left button select");
+            		for(var i =1; i<=BUTTONNUM; i++){
+            	        var x0 = buttonstack[i].x0;
+            	        var y0 = buttonstack[i].y0;
+            	        var x1 = buttonstack[i].x1;
+            	        var y1 = buttonstack[i].y1;
+            	    	gui.fillbox(hdc,x0,y0-15,x1-x0,y1-y0-5);       
+            		}
+            		console.log("left button select end");
+            	}
+            	gui.releasedc(hdc);
+            	break;
+            }
 
-
-            case DOWN:
+            //case DOWN:
             case RIGHT:
-            
+            {
+            	if(buttonnum < BUTTONNUM)
+            		buttonnum++;
+                var hdc = gui.getclientdc();
+                gui.selectbutton(hdc,x0,y0,x1,y1);
+            	gui.releasedc(hdc);
+            	break;
+            }
+
+            case ENTER:
+            {
+            	if(buttonnum == 1){
+            		if(pageindex != 2){
+		    			page2();
+		        		pagestack.push(2);
+		        		pageindex = 2;
+		    		}
+            	}
+            	break;
+            }
           }
        // break;
 	//}
@@ -167,6 +208,7 @@ function page1(){
 	gui.rectangle(hdc,0,0,159,20);
 	gui.rectangle(hdc,0,139,159,159);
 	gui.button(hdc,10,90,74,110,"Function");
+	gui.selectbutton(hdc);
 
     gui.textout(hdc, 30, 2, "Scan Barcode");
 	gui.textout(hdc,30,141,"Status: start ...");
@@ -190,12 +232,20 @@ gui.button = function Button(hdc,x0,y0,x1,y1,str){
 	gui.drawtext(hdc,x0,y0+2,x1,y1,str,gui.drawtext.DT_CENTER);
 }
 var buttonstack = new Array();
-var buttonnum = 1;
+buttonstack[1] = {x0:10,y0:90,x1:74,y1:110};
+var buttonnum = 0;
 
 /* selected button*/
-gui.selectbutton = function(hdc,x0,y0,x1,y1){
-	gui.moveto(hdc,x0,y0);
-	gui.lineto(hdc,x1,y1);
-	gui.moveto(hdc,x0,y1);
-	gui.lineto(hdc,x1,y0);
+gui.selectbutton = function(hdc/*,x0,y0,x1,y1*/){
+	for(var i =1; i<=BUTTONNUM; i++){
+        var x0 = buttonstack[i].x0;
+        var y0 = buttonstack[i].y0;
+        var x1 = buttonstack[i].x1;
+        var y1 = buttonstack[i].y1;
+        if(i == buttonnum){
+            gui.drawtext(hdc,x0,y0-15,x1,y1-20,"***",gui.drawtext.DT_CENTER);
+        }else{
+			gui.fillbox(hdc,x0,y0-15,x1-x0,y1-y0-5);
+        }
+    }
 }
